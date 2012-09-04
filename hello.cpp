7422e2f6056b0231c86fffb35ffd36beeb040f34
Steven Buhrman <scbuhrma@unca.edu>
2012-09-04 18:35:57 -0400

    Update hello.cpp to solver Euler problem 1

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-
-using std::cout;
-using std::endl;
-
-int euler(int n);
+using namespace std;
 
 int main(){
-	cout << "hello world!" << endl;
-	return 0;	
+int sum =0;
+	for (int i=0; i<1000;++i)
+	{
+		if (i%3 == 0 || i%5 == 0)
+			sum +=i;
+	}
+	cout << sum << endl;
 }
-
