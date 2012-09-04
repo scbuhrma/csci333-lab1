b2e9d15b3e8e5d0a5affadea6b0a8dca97c6a0ce
Steven Buhrman <scbuhrma@unca.edu>
2012-09-04 19:56:28 -0400

    updated problem 2

--- problem2.cpp
+++ problem2.cpp
@@ -3,19 +3,18 @@ using namespace std;
 
 long Fibonacci(long);
 long Fibonacci (long t){
-
 	long sum = 0;
 	long fibseq = 0;
 	long a = 0;
 	long b = 1;
 	if ( t <= 1)
 		return 0;	
-	for (long i; i<=t; ++i){
+	for (long i=0; i<=t; ++i){
 	
 	fibseq = a+b;
 	a=b;
 	b=fibseq;
-	if(fibseq %2 == 0)
+	if(fibseq %2 == 0 &&  fibseq<=4000000)
 		sum += fibseq;	
 
 	}
@@ -27,7 +26,7 @@ long Fibonacci (long t){
 int main(){
 
 	long fibsum;
-	fibsum = Fibonacci(1000);
+	fibsum = Fibonacci(11);
 	cout<<fibsum<<endl;
 	return 0;
 
