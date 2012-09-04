f699d8dfb30243ab91f4cdce8757a2c6e9126a61
Steven Buhrman <scbuhrma@unca.edu>
2012-09-04 18:28:21 -0400

    update to README

--- lab1.cpp
+++ lab1.cpp
@@ -7,17 +7,6 @@ int euler(int n);
 
 int main(){
 	cout << "hello world!" << endl;
-	cout << "result: " << euler(1000) << endl;
-	return 0;
+	return 0;	
 }
 
-int euler(int n){
-	int sum = 0;
-
-	for(int i = 0; i < n; ++i){
-	 if(i%5 == 0|| i%7 == 0){
-	  sum +=i;
-	 }
-	}
-	return sum;
-}
